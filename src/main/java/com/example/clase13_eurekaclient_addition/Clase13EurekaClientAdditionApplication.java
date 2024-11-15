7502915460394b13d355f76a2bf9c614ab36c724
oscar-diaz <oscarpucp@gmail.com>
2024-11-15 17:58:15 -0500

    Clase 13 - Gateway y Resiliencia

---
+++ src/main/java/com/example/clase13_eurekaclient_addition/Clase13EurekaClientAdditionApplication.java
@@ -0,0 +1,14 @@
+package com.example.clase13_eurekaclient_addition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Clase13EurekaClientAdditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase13EurekaClientAdditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clase13_eurekaclient_addition/Clase13ClientEurekaAdditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase13_eurekaclient_addition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase13ClientEurekaAdditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
